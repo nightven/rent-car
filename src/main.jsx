b1bb81725adf966dc04488704e7240e06fb0744f
Виталий Беяр <vitaliy.beyar@gmail.com>
2024-02-14 08:11:09 -0330

    fixed rout

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from "./components/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/rent-car">
       <App />
     </BrowserRouter>
   </React.StrictMode>
